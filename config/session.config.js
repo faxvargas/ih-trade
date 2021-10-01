9c59caa6c30ef3e2e79845ebc2292836908e1038
faxvargas <fab.med1011@gmail.com>
2021-10-01 10:13:50 -0500

    se alargo la cokie

--- config/session.config.js
+++ config/session.config.js
@@ -15,7 +15,7 @@ const generateSession = (app) => {
                 sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
                 secure: process.env.NODE_ENV === "production",
                 httpOnly: true,
-                maxAge: 60000 
+                maxAge: 60000000000000000000 
             },
             store: MongoStore.create({
                 mongoUrl: process.env.MONGODB_URI
